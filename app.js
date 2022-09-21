163ce2538c003d6eb6078735a3c6a60058c6451d
GumiHo <hemchanmetrey123@gmail.com>
2022-09-21 16:44:41 +0700

    today

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const server = http.createServer((req,res)=>{
         res.end('Home page')
     }
     if(req.url === '/about'){
+        //Blocking Code
+        for(let i = 0; i< 1000; i++){
+            for(let j = 0; j< 1000; j++){
+                console.log(`${i} ${j}`)
+            }
+        }
         res.end('About page')
     }
     res.end('Error page')
