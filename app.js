cab25b39cc72a61eadc38e199d50d3aa19540dd9
GumiHo <hemchanmetrey123@gmail.com>
2022-09-21 11:08:00 +0700

    hhh

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-// Modules - Encapsulate code (only share minimun)
-// CommonJS every file is module (by default)
-// const names = require('./4-name')
-// const sayHI = require('./5-utiil')
-// console.log(names)
-// console.log(sayHI)
-// sayHI('khim')
-// sayHI(names.peter)
-// sayHI(names.john)
-// const data = require('./6_alternative_flavor')
-// console.log(data)
-// const { readFile, writeFile} = require('fs')
-// readFile('./content/first.txt',(err,result)=> {
-//     if(err){
-//         console.log(err)
-//     }
-//     console.log(result)
-// })
-const _= require('lodash');
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
-console.log("Gumiho handsome boy")
-const Module = require('module')
-console.log(Module.builtinModules)
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.end('Home page')
+    }
+    if(req.url === '/about'){
+        res.end('About page')
+    }
+    res.end('Error page')
+
+})
+server.listen(8080,()=>{
+    console.log('Sever is listening on port 8080.....')
+})
+
 
